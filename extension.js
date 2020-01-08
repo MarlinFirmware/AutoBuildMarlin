029be40353fd471b26b80736b1588f0c92c0bc49
Giuliano Zaro <3684609+GMagician@users.noreply.github.com>
2020-01-08 02:32:39 +0100

    Fix Windows compatibility (#5)

--- extension.js
+++ extension.js
@@ -2,46 +2,31 @@
 
 var vscode = require('vscode');
 
-function activate(context) {
+exports.activate = function(context) {
+  //console.log(vscode);
 
-  console.log('Extension "AutoBuildMarlin" is now active!');
-
-  const AUTO_CMD1 = 'export TK_SILENCE_DEPRECATION=1 && [[ -f "buildroot/share/vscode/auto_build.py" ]] && python buildroot/share/vscode/auto_build.py',
-        AUTO_CMD2 = '|| python buildroot/share/atom/auto_build.py';
+  // Figure out where auto_build.py is located
+  var fs = require('fs');
+  var AUTO_CMD = 'buildroot/share/atom/auto_build.py';
+  fs.access(vscode.workspace.rootPath + '/' + AUTO_CMD, fs.constants.F_OK, function (err) {
+    if (err) AUTO_CMD = 'buildroot/share/vscode/auto_build.py';
+  });
 
   var NEXT_TERM_ID = 1;
-  var mf_build     = vscode.commands.registerCommand('mfbuild',     function () {
-    vscode.commands.executeCommand('workbench.action.files.saveAll');
-    const terminal = vscode.window.createTerminal(`Marlin Build #${NEXT_TERM_ID++}`);
+  var abm_command = function(cmd, nosave) {
+    if (!nosave) vscode.commands.executeCommand('workbench.action.files.saveAll');
+    var title = cmd.charAt(0).toUpperCase() + cmd.slice(1);
+    var terminal = vscode.window.createTerminal(`Marlin ${title} #${NEXT_TERM_ID++}`);
     terminal.show(true);
-    terminal.sendText(`${AUTO_CMD1} build ${AUTO_CMD2} build`);
-  });
-  var mf_upload    = vscode.commands.registerCommand('mfupload',    function () {
-    vscode.commands.executeCommand('workbench.action.files.saveAll');
-    const terminal = vscode.window.createTerminal(`Marlin Upload #${NEXT_TERM_ID++}`);
-    terminal.show(true);
-    terminal.sendText(`${AUTO_CMD1} upload ${AUTO_CMD2} upload`);
-  });
-  var mf_traceback = vscode.commands.registerCommand('mftraceback', function () {
-    vscode.commands.executeCommand('workbench.action.files.saveAll');
-    const terminal = vscode.window.createTerminal(`Marlin Traceback #${NEXT_TERM_ID++}`);
-    terminal.show(true);
-    terminal.sendText(`${AUTO_CMD1} traceback ${AUTO_CMD2} traceback`);
-  });
-  var mf_clean     = vscode.commands.registerCommand('mfclean',     function () {
-    const terminal = vscode.window.createTerminal(`Marlin Clean #${NEXT_TERM_ID++}`);
-    terminal.show(true);
-    terminal.sendText(`${AUTO_CMD1} clean ${AUTO_CMD2} clean`);
-  });
+    terminal.sendText(`python ${AUTO_CMD} ${cmd}`);
+  };
 
-  context.subscriptions.push(mf_build);
-  context.subscriptions.push(mf_upload);
-  context.subscriptions.push(mf_traceback);
-  context.subscriptions.push(mf_clean);
-}
-exports.activate = activate;
+  context.subscriptions.push(vscode.commands.registerCommand('mfbuild', function(){ abm_command('build'); }));
+  context.subscriptions.push(vscode.commands.registerCommand('mfupload', function(){ abm_command('upload'); }));
+  context.subscriptions.push(vscode.commands.registerCommand('mftraceback', function(){ abm_command('traceback'); }));
+  context.subscriptions.push(vscode.commands.registerCommand('mfclean', function(){ abm_command('clean', true); }));
+};
 
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
+exports.deactivate = function() {
+
+};
