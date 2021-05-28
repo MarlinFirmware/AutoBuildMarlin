681284a9e23d8066ecd94ea1a0b6ad00e699dc3e
Scott Lahteine <github@thinkyhead.com>
2021-05-28 15:27:48 -0500

    :bug: [2.1.33] Fix 'Show on Startup' opening platformio.ini
    This appears to be a PlatformIO IDE issue. The workaround is to allow PIO to finish initialization before showing the panel, which is a good idea anyway.

--- extension.js
+++ extension.js
@@ -29,7 +29,7 @@ exports.activate = (context) => {
   abm.watchAndValidate();
   abm.set_context('active', true);
 
-  if (abm.show_on_startup()) abm.run_command();
+  if (abm.show_on_startup()) setTimeout(abm.run_command, 1000);
 };
 
 exports.deactivate = () => { abm.set_context('active', false); };
